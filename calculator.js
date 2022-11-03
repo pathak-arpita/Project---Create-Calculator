15897123cd35e42ae04311b15101ff6750fe82f3
Arpita <pathak.arpita6497@gmail.com>
2022-11-03 20:33:43 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let str = " ";
+let buttons = document.querySelectorAll(".cal-btn");
+Array.from(buttons).forEach((numbers) => {
+    numbers.addEventListener("click", (e) => {
+        if (e.target.innerHTML == '=') {
+            str = eval(str);
+            document.querySelector('input').value = str;
+        }
+
+        else if (e.target.innerHTML == 'AC') {
+            str = " ";
+            document.querySelector('input').value = str;
+        }
+
+        else {
+            console.log(e.target);
+            str = str + e.target.innerHTML;
+            document.querySelector('input').value = str;
+        }
+
+    })
+})
